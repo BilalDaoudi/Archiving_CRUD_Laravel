75b19e51a6b7380a1fb8d6f80a75bc9a00999eec
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-06-22 14:13:58 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
