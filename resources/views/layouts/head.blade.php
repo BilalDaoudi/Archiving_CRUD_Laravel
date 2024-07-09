785ac8c238db175f925d430ca154af39c50d4945
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-07-09 14:39:50 +0100

    Update head.blade.php

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,6 +1,4 @@
 <!-- Title -->
 <title>@yield("title")</title>
-
 <link href="{{ URL::asset('assets/css/ltr.css') }}" rel="stylesheet">
-
 <link href="{{ URL::asset('assets/css/blugins/bootstrap.min.css') }}" rel="stylesheet">
