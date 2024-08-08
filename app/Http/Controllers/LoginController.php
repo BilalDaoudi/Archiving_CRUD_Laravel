e87a05bfa14412c092bc3d195b839322fd585fcf
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-08-08 09:35:29 +0100

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,15 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
+    
     public function index(){
         return view("login");
     }
+    
     public function login(Request $req){
         echo $req->email;
         echo $req->password;
