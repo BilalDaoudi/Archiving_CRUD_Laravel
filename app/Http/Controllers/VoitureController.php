82999f887b5eb76ca1f900b0f67a8590fe6ce830
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-08-08 09:35:51 +0100

    Update VoitureController.php

--- app/Http/Controllers/VoitureController.php
+++ app/Http/Controllers/VoitureController.php
@@ -1,26 +1,32 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 
 class VoitureController extends Controller
 {
+    
     public function index()
     {
         //
     }
+    
     public function create()
     {
         //
     }
+    
     public function store(Request $request)
     {
         //
     }
+    
     public function show($id)
     {
         //
     }
+    
     public function edit($id)
     {
         //
