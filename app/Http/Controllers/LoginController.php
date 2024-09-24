8fdd03cb0f6dc0f69a35f6f780d2dc69f65e15ce
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-09-24 20:11:07 +0100

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,24 +8,17 @@ use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
+
+
     
     public function index(){
         return view("login");
     }
+
+
     
     public function login(Request $req){
         echo $req->email;
         echo $req->password;
-        // $T_values = [
-        //     "email" => $req->email,
-        //     "password" => $req->password
-        // ];
-        // if(Auth::attempt($T_values)){
-        //     $req->session()->regenerate();
-        //     return to_route('dashbord');
-        // }
-        // else{
-        //     return back();
-        // }
     }
 }
